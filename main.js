4f542c496a45ccdfbf5e6f9749981971f215d609
hasan-kayan <hasankayan2000@hotmail.com>
2024-03-12 11:17:33 +0300

    Having fun

---
+++ main.js
@@ -0,0 +1,6 @@
+// JavaScript code goes here
+
+window.onload = function() {
+    var h1Element = document.querySelector('h1');
+    h1Element.style.color = 'red';
+}
---
+++ src/navbar/navbar.js
@@ -0,0 +1,26 @@
+// JavaScript code goes here
+
+// Add event listeners to each button
+document.getElementById("btn1").addEventListener("click", function() {
+    playMusic();
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+    playMusic();
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+    playMusic();
+});
+
+// Function to play the music
+function playMusic() {
+    var audio = document.getElementById("audio");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0; // Reset the music to the beginning
+        audio.play();
+    }
+}
